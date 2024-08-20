b7edefc7d6b72251dab564010b4721ef09af2223
MateoGL <mateo.gil@hotmail.com>
2024-08-19 22:32:50 -0500

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ee531cdc-97b1-4163-bb9d-59a9c88b2aee';
+export const MODAL_CLOSE_SEC = 2.5;
